8aff1dc573b7464bff32fb24a9a7faabc6d73d55
Lucasrafael703 <165857821+marianabaleeiro@users.noreply.github.com>
2024-04-06 15:33:13 -0300

    Quarto Comit

--- CA/urls.py
+++ CA/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import soma_quantidade_arrecadacao
 
 urlpatterns = [
-    path('soma-arrecadacao/', soma_quantidade_arrecadacao, name='soma_arrecadacao'),
+    path('', soma_quantidade_arrecadacao, name='soma_arrecadacao'),
 ]
