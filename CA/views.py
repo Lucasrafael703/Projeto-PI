909702c2777bf5dabe8e8dc26b0a1342383845d4
Lucasrafael703 <165857821+marianabaleeiro@users.noreply.github.com>
2024-05-14 22:30:40 -0300

    Criação do contador na pagina inicial

--- CA/views.py
+++ CA/views.py
@@ -6,10 +6,5 @@ def soma_quantidade_arrecadacao(request):
     # Calcula a soma da quantidade de arrecadação
     soma = Arrecadacao.objects.aggregate(soma_quantidade=Sum('quantidade'))['soma_quantidade']
     context = {'soma_quantidade': soma}
-    return render(request, 'soma_quantidade_arrecadacao.html', context)
-
-def soma(request):
-    # Calcula a soma da quantidade de arrecadação
-    soma = Arrecadacao.objects.aggregate(soma_quantidade=Sum('quantidade'))['soma_quantidade']
-    context = {'soma_quantidade': soma}
     return render(request, 'index.html', context)
+
