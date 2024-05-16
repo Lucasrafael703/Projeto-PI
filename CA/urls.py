52e9ffbe369df6f929eae82968223a3809d1f323
Lucasrafael703 <165857821+marianabaleeiro@users.noreply.github.com>
2024-05-15 23:07:24 -0300

    Integração com o Login

--- CA/urls.py
+++ CA/urls.py
@@ -4,5 +4,6 @@ from .views import pontos
 
 urlpatterns = [
     path('index/', soma_quantidade_arrecadacao, name='soma_arrecadacao'),
+    path('', soma_quantidade_arrecadacao, name=''),
     path('pontos/', pontos, name='pontos'),
 ]
