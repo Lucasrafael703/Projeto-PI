0ec4982c1f1a1254d9f1f72008b2b82ac28de37c
Lucasrafael703 <165857821+marianabaleeiro@users.noreply.github.com>
2024-05-16 21:17:14 -0300

    Corrigindo json conteudo

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 
+# Collect static files
+echo "Collect static files"
+python3.9 manage.py collectstatic --noinput
 
-python3 manage.py collectstati
+# Migrate database
+echo "Apply database migrations"
+python3.9 manage.py migrate
